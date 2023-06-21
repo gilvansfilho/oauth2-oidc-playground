d483f5bb2ff2ef8496f1e6b02db21fc38310d738
Gilvan Filho <gfilho@redhat.com>
2023-06-20 22:25:55 -0300

    Melhorias...

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-podman run -d --name sso -p 8282:8080 -e SSO_ADMIN_USERNAME=admin -e SSO_ADMIN_PASSWORD=admin custom-sso
-podman run -d --name httpd -p 8080:8080 -v ${PWD}/html:/var/www/html registry.access.redhat.com/ubi8/httpd-24:1-263.1684490927
+podman run -d --name sso -p 8282:8080 -e SSO_ADMIN_USERNAME=admin -e SSO_ADMIN_PASSWORD=admin registry.redhat.io/rh-sso-7/sso76-openshift-rhel8:latest
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    podman run -d --rm --name httpd -p 8080:8080 -v ${PWD}/html:/var/www/html:Z registry.access.redhat.com/ubi8/httpd-24:latest
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    podman run -d --rm --name httpd -p 8080:8080 -v ${PWD}/html:/var/www/html registry.access.redhat.com/ubi8/httpd-24:latest
+else
+     podman run -d --rm --name httpd -p 8080:8080 -v ${PWD}/html:/var/www/html registry.access.redhat.com/ubi8/httpd-24:latest
+fi
